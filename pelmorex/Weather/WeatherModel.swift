6459c52d5686841f13fc47f6d9bd395d453b8b5c
mert.koksal <mert.koksal@ordulu.com>
2022-09-05 10:15:02 +0300

    values.decode moved to VM

--- pelmorex/Weather/WeatherModel.swift
+++ pelmorex/Weather/WeatherModel.swift
@@ -23,31 +23,4 @@ struct WeatherModel: Codable, Hashable {
         case lbl_updatetime, updatetime, updatetime_stamp_gmt, wxcondition, icon, inic, temperature, feels_like, temperature_unit, placecode
     }
     
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        lbl_updatetime = try values.decode(String.self, forKey: .lbl_updatetime)
-        updatetime = try values.decode(String.self, forKey: .updatetime)
-        updatetime_stamp_gmt = try values.decode(String.self, forKey: .updatetime_stamp_gmt)
-        wxcondition = try values.decode(String.self, forKey: .wxcondition)
-        icon = try values.decode(String.self, forKey: .icon)
-        inic = try values.decode(String.self, forKey: .inic)
-        temperature_unit = try values.decode(String.self, forKey: .temperature_unit)
-        placecode = try values.decode(String.self, forKey: .placecode)
-        // decode temperature, when it comes as a String
-        if let theString = try? values.decode(String.self, forKey: .temperature),
-           let temp = Int(theString) {
-            temperature = temp
-        } else {
-            temperature = try values.decode(Int.self, forKey: .temperature)
-        }
-        
-        // decode feels_like, when it comes as a String
-        if let theString = try? values.decode(String.self, forKey: .feels_like),
-           let feel_temp = Int(theString) {
-            feels_like = feel_temp
-        } else {
-            feels_like = try values.decode(Int.self, forKey: .feels_like)
-        }
-        
-    }
 }
--- pelmorex/Weather/WeatherViewModel.swift
+++ pelmorex/Weather/WeatherViewModel.swift
@@ -16,11 +16,16 @@ class WeatherViewModel: Decodable {
     private(set) var wxcondition: String?
     private(set) var icon: String?
     private(set) var inic: String?
-    private(set) var temperature: String?
-    private(set) var feels_like: String?
+    private(set) var temperature: Int?
+    private(set) var feels_like: Int?
     private(set) var temperature_unit: String?
     private(set) var placecode: String?
-//    private(set) var cityName: String?
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case lbl_updatetime, updatetime, updatetime_stamp_gmt, wxcondition, icon, inic, temperature, feels_like, temperature_unit, placecode
+    }
+    //    private(set) var cityName: String?
     
     init(model: WeatherModel) {
         self.weatherModel = model
@@ -30,10 +35,38 @@ class WeatherViewModel: Decodable {
         self.wxcondition = model.wxcondition
         self.icon = model.icon
         self.inic = model.inic
-        self.temperature = String(model.temperature)
-        self.feels_like = String(model.feels_like)
+        self.temperature = Int(model.temperature)
+        self.feels_like = Int(model.feels_like)
         self.temperature_unit = model.temperature_unit
         self.placecode = model.placecode
-//        self.cityName = getCityName()
+        //        self.cityName = getCityName()
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        lbl_updatetime = try values.decode(String.self, forKey: .lbl_updatetime)
+        updatetime = try values.decode(String.self, forKey: .updatetime)
+        updatetime_stamp_gmt = try values.decode(String.self, forKey: .updatetime_stamp_gmt)
+        wxcondition = try values.decode(String.self, forKey: .wxcondition)
+        icon = try values.decode(String.self, forKey: .icon)
+        inic = try values.decode(String.self, forKey: .inic)
+        temperature_unit = try values.decode(String.self, forKey: .temperature_unit)
+        placecode = try values.decode(String.self, forKey: .placecode)
+        // decode temperature, when it comes as a String
+        if let theString = try? values.decode(String.self, forKey: .temperature),
+           let temp = Int(theString) {
+            temperature = temp
+        } else {
+            temperature = try values.decode(Int.self, forKey: .temperature)
+        }
+        
+        // decode feels_like, when it comes as a String
+        if let theString = try? values.decode(String.self, forKey: .feels_like),
+           let feel_temp = Int(theString) {
+            feels_like = feel_temp
+        } else {
+            feels_like = try values.decode(Int.self, forKey: .feels_like)
+        }
+        
     }
 }
