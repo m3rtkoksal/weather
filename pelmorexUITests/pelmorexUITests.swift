5af042c89216cc48befec55f2cf6e6164247e4ea
mert.koksal <mert.koksal@ordulu.com>
2022-09-08 14:17:26 +0300

    another ui test and rain icon

--- pelmorexUITests/pelmorexUITests.swift
+++ pelmorexUITests/pelmorexUITests.swift
@@ -13,7 +13,7 @@ class pelmorexUITests: XCTestCase {
             let app = XCUIApplication()
             app.launch()
             
-            let questionButton =  app/*@START_MENU_TOKEN@*/.staticTexts["Question?"]/*[[".buttons[\"Question?\"].staticTexts[\"Question?\"]",".staticTexts[\"Question?\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            let questionButton = app/*@START_MENU_TOKEN@*/.staticTexts["Question?"]/*[[".buttons[\"Question?\"].staticTexts[\"Question?\"]",".staticTexts[\"Question?\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
             let weatherDetailPage = app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .textField).element
             let checkButton = app.buttons["Check"]
             let emailTFChecked =  app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .textField).element
@@ -23,6 +23,31 @@ class pelmorexUITests: XCTestCase {
             checkButton.tap()
             emailTFChecked.tap()
             XCTAssert(emailTFChecked.exists)
+    }
+    
+    func testDegreeSegment() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        let questionButtonLo = app/*@START_MENU_TOKEN@*/.staticTexts["Question?"]/*[[".buttons[\"Question?\"].staticTexts[\"Question?\"]",".staticTexts[\"Question?\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let tf = app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1)
+        let tfEmail = app.staticTexts["Email"]
+        let button = app.buttons["Check"]
+        let nb = app.navigationBars["pelmorex.QuestionVC"].buttons["Back"]
+        let segOne =  app/*@START_MENU_TOKEN@*/.buttons["°F"]/*[[".segmentedControls.buttons[\"°F\"]",".buttons[\"°F\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let segZero = app/*@START_MENU_TOKEN@*/.buttons["°C"]/*[[".segmentedControls.buttons[\"°C\"]",".buttons[\"°C\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+      
+        
+        questionButtonLo.tap()
+        tf.tap()
+        tfEmail.tap()
+        button.tap()
+        nb.tap()
+        segOne.tap()
+        segZero.tap()
+        XCTAssert(segZero.exists)
+        
+        
     }
     
 }
